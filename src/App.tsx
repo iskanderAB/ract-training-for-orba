dc210758934026485ead8eb65027d3ec1831732f
iskanderab <iskanderabasi@gmail.com>
2022-08-15 21:37:38 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Square from './components/Square/Square';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Square/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Square/Square.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Square.css'
+
+const Square = () => {
+    return (
+        <p className='saif'> 
+            hello saif ♥ 
+        </p>
+    )
+}
+
+export default Square;
