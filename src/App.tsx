2d7d4b0a48359321de9b5b47039d9dcf07065927
iskanderab <iskanderabasi@gmail.com>
2022-08-17 21:43:12 +0100

    add list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import Square from './components/Square/Square';
 import './App.css';
 
 function App() {
+  const [list , setList] = useState([
+    {key : 1 , value: "saif"},
+    {key : 2 , value: "hammadi"},
+    {key : 3 , value: "ali"},
+    {key : 4 , value: "khaled"},  
+  ])
+
+  const deleteSquare = (index:number) => {
+      setList(list.filter((v,i) => i !== index))
+  }
   return (
     <div className="App">
-        <Square/>
+      {
+        list.map(function(value,index) { 
+              return (<div 
+                      key={value.key}
+                      onClick={function(){deleteSquare(index)}}>
+                        <Square myIndex={value.value} />
+                    </div>)
+      } )
+      }
     </div>
   );
 }
--- src/components/Square/Square.tsx
+++ src/components/Square/Square.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Square.css'
 
-const Square = () => {
+const Square = ({myIndex}:{myIndex: string}) => {
+    // const [ctr , setCtr] = useState('#010');
+    // const [ctr1 , setCtr1] = useState('#010');
+
+    // useEffect(()=>{
+    //     console.log("start with ♥");
+    // },[ctr,ctr1])
+
+    // const editCtr = () => {
+    //         setCtr(ctr === '#F10' ? '#010' : '#F10' );  
+    // }
     return (
-        <p className='saif'> 
-            hello saif ♥ 
+        <p 
+            className='saif'
+            // onClick={editCtr}
+            // style={{
+            //     backgroundColor : ctr
+            // }}
+        > 
+           {myIndex}
         </p>
     )
 }
